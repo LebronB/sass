7df016fac2db261393027cfd45c729c81c2a6715
LebronB <2687877991@qq.com>
2021-05-19 10:42:37 +0800

    register template

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "mainapp"
 
+# 主url中设置了namespace，反向解析时应为"mainapp: register"
 urlpatterns = [
     # 试用腾讯云短信
     path('sms/', views.send_massage, name='sms'),
--- ssas/urls.py
+++ ssas/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mainapp/', include('mainapp.urls', namespace='mainapp')),
+    path('', include('mainapp.urls', namespace='mainapp')),
 ]
